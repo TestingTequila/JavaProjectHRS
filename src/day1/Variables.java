f6b6dc7dc1f1ae41b513575db7b7e8d4f7255776
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-05-08 07:43:12 +0530

    Data Type Concept

---
+++ src/day1/Variables.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        //byte x = 10; 1 byte or 8 bits
+        //short x = 10; 2 byte or 16 bits
+        //int x = 10; 4 byte or 32 bits
+        //long x = 10; 8 byte or 64 bits
+
+        long mobileNumber=  8860418225L;
+    }
+}
---
+++ src/day2/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte b = 127; // -128 to 127
+
+        short s = 32767; //-32768 to 32767
+
+        int i = 2147483647;//-2147483648  to 2147483647;
+
+        long l = 9223372036854775807L; //-9223372036854775808 to 9223372036854775807
+
+        //WAP to add two integer numbers [10, 20]
+
+        byte a = 100;
+        byte c = 30;
+
+        int sum = a + c;
+
+
+    }
+}
