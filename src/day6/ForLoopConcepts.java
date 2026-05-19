224221b383a500cf722b6b329918db25b9994c61
Ashish Mishra <ashish.mishra.sse@gmail.com>
2026-05-19 07:28:40 +0530

    Loops-while & for loop

---
+++ src/day6/ForLoopConcepts.java
@@ -0,0 +1,39 @@
+package day6;
+
+public class ForLoopConcepts {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("=======1. WAP to Print my name 10 times==================");
+
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Ashish");
+        }
+
+
+        System.out.println("=======2. WAP to Print my numbers from 1-10 ==================");
+
+        for (int j = 1; j <= 10; j++) {
+            System.out.println(j);
+
+        }
+
+        System.out.println("=======3. WAP to Print my numbers from 10-1 ==================");
+
+
+        for (int k = 10; k > 0; k--) {
+            System.out.println(k);
+
+        }
+
+
+        System.out.println("=======3. WAP to Print table of 2 ==================");
+        for (int m = 1; m <= 10; m++) {
+            System.out.println(2 * m);
+
+        }
+
+    }
+}
---
+++ src/day6/WhileLoopConcepts.java
@@ -0,0 +1,43 @@
+package day6;
+
+public class WhileLoopConcepts {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("=======1. WAP to Print my name 10 times==================");
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Ashish");
+            i++;//11
+        }
+
+
+        System.out.println("=======2. WAP to Print my numbers from 1-10 ==================");
+
+        int j = 1;
+        while (j <= 10) {
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println("=======3. WAP to Print my numbers from 10-1 ==================");
+
+        int k = 10;
+        while (k > 0) {
+            System.out.println(k);
+            k--;
+        }
+
+
+        System.out.println("=======3. WAP to Print table of 2 ==================");
+
+        int m = 1;
+        while (m <= 10) {
+            System.out.println(2 * m);
+            m++;//11
+        }
+
+    }
+}
